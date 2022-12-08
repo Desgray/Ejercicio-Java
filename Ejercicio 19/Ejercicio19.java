72cb9d3baa9337fe6b23601d4d2cd868cc2d6636
Samuel Narvaja Torres <samuel.narvaja.torres@gmail.com>
2022-12-07 22:07:32 -0300

    Cuarta version

--- Ejercicio 19/Ejercicio19.java
+++ Ejercicio 19/Ejercicio19.java
@@ -1,65 +1,75 @@
-import java.util.Scanner;
-public class Ejercicio19{
-    public static void main(String[] args){
-            Scanner valor = new Scanner(System.in);
-            int filas,colum;
-            System.out.print  ("Ingrese filas: ");
-            filas=valor.nextInt();
-            System.out.print("Ingrese columnas ");
-            colum=valor.nextInt();
-            while(filas!=colum){
-                System.out.print("Las columnas tienen que se iguales a las filas: ");
-                System.out.print("Ingrese Filas: ");    
-                filas=valor.nextInt();
-            }
-            int [][] mat = new int[filas][colum];
-            matrix2(mat,filas);
-            mostrar(mat,filas);
-            AntiSimetrica(mat,filas);
-    }   // cierre del main
 
-    
-public static void AntiSimetrica(int mat[][], int fila){
-    boolean bandera;
-    bandera=false;
-    for (int i=0;i<fila ;i++ ) {
-        for (int j=0;j<fila ;j++ ) {
-            if(i==j){
-                if(mat[i][j]==0){
-                    bandera=true;
-                }else{
-                    bandera=false;
-                }
+import java.util.Scanner;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Ejercicio19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*Realice un programa que compruebe si una matriz dada es anti simétrica. Se dice que una
+        matriz A es anti simétrica cuando ésta es igual a su propia traspuesta, pero cambiada de
+        signo. Es decir, A es anti simétrica si A = -AT. La matriz traspuesta de una matriz A se
+        denota por AT y se obtiene cambiando sus filas por columnas (o viceversa).
+        */
+        
+        int[][] mat = new int[3][3];
+        int[][] matTras = new int[3][3];
+        matriz(mat);
+        System.out.println();
+        Traspuesta(mat, matTras);
+        Antisimetrica(mat,matTras);
+    }
+    public static void Antisimetrica(int[][] mat,int[][] matTras){
+        int cont=0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <3; j++) {
+                if(matTras[i][j]*-1 != mat[i][j]){     
+                        cont=cont+1;
             }
         }
     }
-     if(bandera==true){
-            System.out.print("La matriz es Anti Simetrica");
-     }else{
-            System.out.print("La matriz no es Anti Simetrica");
-     }
-}
-public static void  matrix2(int[][] mat,int fila){
-    Scanner result = new Scanner(System.in);
-    for (int i=0;i<fila ;i++ ) {
-        for (int j=0;j<fila ;j++ ) {
-            System.out.print("Ingrese numero: ");
-            mat[i][j]=result.nextInt();
-            while(mat[i][j]<0 || mat[i][j]>=10){
-                System.out.print("Ingrese numero entre 1 y 9 : ");
-                mat[i][j]=result.nextInt();
+        if(cont==0){
+            System.out.println("Matriz antisimetrica");
+        }else{
+            System.out.println("No es antisimetrica");
+        }
+   }
+    
+    public static void Traspuesta(int[][] mat,int[][] matTras){
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                matTras[j][i] = mat[i][j];
             }
-         }
+        }
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("["+matTras[i][j]+"]");
+            }
+            System.out.println();
+        }
     }
 
+    public static void matriz(int[][] mat) {
+        Scanner valor = new Scanner(System.in);
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Ingrese valor: ");
+                mat[i][j] = valor.nextInt();
+            }
+        }
 
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("["+mat[i][j]+"]");
+            }
+            System.out.println();
+        }
+    }
 }
- public static void  mostrar(int[][] matrix,int aux){
-      for ( int i=0;i<aux ; i++) {
-          for ( int j=0;j<aux ;j++ ) {
-            System.out.print(" "+matrix[i][j]);
-          }
-          System.out.println();
-        }  
-  }
-}  // cierre de la clase
